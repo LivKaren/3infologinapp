7d287ab633d93728828ad03c132f6ec4c8fd2d08
LivKaren <livgilgen@gmail.com>
2024-05-22 14:51:16 -0300

    pronto

---
+++ src/screens/InsertNote.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { TextInput } from "react-native-paper";
+
+export default function InsertNote() {
+  const [text, setText] = useState("");
+  return (
+    <>
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        placeholder="Inserir nota"
+        right={<TextInput.Icon name="send" />}
+      />
+    </>
+  );
+}
